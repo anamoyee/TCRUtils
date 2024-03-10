21ce359ce70b506e2aa5532634d984c7a2bb5ed5
TheCreatorrr333 <thecreatorrr333@gmail.com>
2024-03-10 01:21:28 +0100

    terminal: remove colors actually

--- tcrutils/src/tcr_terminal.py
+++ tcrutils/src/tcr_terminal.py
@@ -21,25 +21,6 @@ class Cursor:
     else:
       _send(a)
 
-class TerminalColor:
-  @staticmethod
-  def set_terminal_rgb(r: int, g: int, b: int):
-    """
-    Sets the terminal text color to the specified RGB value.
-
-    Parameters:
-        r: int, The red component (0-255).
-        g: int, The green component (0-255).
-        b: int, The blue component (0-255).
-    """
-    if not all(isinstance(x, int) for x in (r, g, b)):
-      raise ValueError('All RGB values must be integers.')
-
-
-    color_sequence = f"\033[38;2;{r};{g};{b}m"
-
-    print(color_sequence, end='')
-
 class _TerminalSizeTuple(tuple):
   def __new__(cls, width: int, height: int):
     return super().__new__(cls, (width, height))
@@ -68,10 +49,6 @@ class Terminal:
     return _TerminalSizeTuple(*os.get_terminal_size())
 
   cursor = Cursor()
-
-  color = TerminalColor()
-  
-
 
 terminal = Terminal()
 
