eff8f03e4be57295b8933ad8c4819c50cca0bc35
TheCreatorrr333 <thecreatorrr333@gmail.com>
2024-03-22 16:31:21 +0100

    inspect fix

--- tcrutils/src/tcr_inspect.py
+++ tcrutils/src/tcr_inspect.py
@@ -3,7 +3,6 @@ from typing import TypeVar
 
 T = TypeVar('T')
 
-
 def get_lineno(*, default: T | None = None, backtrack_frames: int = 1) -> int | T:
   """Return the python file's line number at which this function was called as an int.
 
@@ -26,4 +25,12 @@ def get_file_colon_lineno(default: T | None = None, backtrack_frames: int = 1, a
     backtrack_frames: int = 1, how many frames to go back in the stack, if set to 1, will return the calling frame's line number, if set to 0 will return this module's internal line number (so dont) but may be useful when building a function that itself makes use of get_lineno() in which case you'd need to make it 2 so your function doesn't just return internal line number
     additional_offset: int = 0, since you cannot easily add to the stringified int, you may set this parameter to offset the returned line number by some amount.
   """
-  return f'{inspect.currentframe().f_back.f_code.co_filename}:{get_lineno(default=default, backtrack_frames=backtrack_frames+1)+additional_offset}'
+
+  frame = inspect.currentframe()
+  for _ in range(backtrack_frames):
+    if frame is None:
+      raise RuntimeError('Invalid frame.')
+    frame = frame.f_back
+
+
+  return f'{frame.f_code.co_filename}:{get_lineno(default=default, backtrack_frames=backtrack_frames+1)+additional_offset}'
