c4d1385aef2bb765448a0a76223e8e1077117afd
TheCreatorrr333 <thecreatorrr333@gmail.com>
2024-04-17 19:39:42 +0200

    markdown... i hope this is the last one

--- tcrutils/_version.py
+++ tcrutils/_version.py
@@ -1,5 +1,5 @@
 __version__ = '12.0.150'
-"""Now \\*should\\* not behave like shit with pyinstaller, but idk really.
+"""## Now \\*should\\* not behave like shit with pyinstaller, but idk really.
 
 This it not guaranteed, but strongly believed by me to always be a str with valid version info...
 """
