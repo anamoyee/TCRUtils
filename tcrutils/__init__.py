2a8f13a17ac308e6ae6d14afb109e9fe491ff54b
TheCreatorrr333 <thecreatorrr333@gmail.com>
2024-05-22 00:06:12 +0200

    removed unfinished crap and readded NoInit to scope

--- tcrutils/__init__.py
+++ tcrutils/__init__.py
@@ -30,7 +30,7 @@ from .dr import placeholders as dr_placeholders
 from .src import tcr_joke as joke
 from .src import tcr_types as types
 from .src.tcr_b64 import b64
-from .src.tcr_class import Singleton
+from .src.tcr_class import NoInit, Singleton
 from .src.tcr_classfuncs import get_classname, get_name_classname, get_qualname_classname
 from .src.tcr_compare import able, isdunder
 from .src.tcr_console import breakpoint, console, start_eval_session
--- tcrutils/src/tcr_class.py
+++ tcrutils/src/tcr_class.py
@@ -28,33 +28,6 @@ class Singleton:
     return cls.__singleton_instance__
 
 
-class SingletonForced:
-  """Force single instance at all times by raising an error if one was already created.
-
-  ## Make sure to put the `tcr.Singleton` inheretance before all others!
-  ### ❌ `class A(B, tcr.Singleton)`
-  ### ✅ `class A(tcr.Singleton, B)`
-
-  ```py
-  class SingleTuple(tcr.Singleton, tuple):
-      ...
-
-    tup = SingleTuple(('tup1',))
-    tup2 = SingleTuple(('tup2',)) # Those arguments are effectively ignored
-
-    console(tup)  # -> ('tup1',)
-    console(tup2) # -> ('tup1',)
-  ```
-  """
-
-  __singleton_instance__ = None
-
-  def __new__(cls, *args, **kwargs) -> Self:
-    if cls.__singleton_instance__ is None:
-      cls.__singleton_instance__ = super().__new__(cls, *args, **kwargs)
-    return cls.__singleton_instance__
-
-
 class NoInit:
   def __init__(self, *args, **kwargs) -> None:
-    raise NotImplementedError('You cannot instiantiate this class')
+    raise RuntimeError('You cannot instiantiate this class')
