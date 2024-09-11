771e650bb72a6238cc26db897aab06dde54b1a7e
TheCreatorrr333 <thecreatorrr333@gmail.com>
2024-09-11 16:03:21 +0200

    version bump after indent change

--- tcrutils/_version.py
+++ tcrutils/_version.py
@@ -1,4 +1,4 @@
-__version__ = '12.0.267'
+__version__ = '12.0.268'
 """## Now \\*should\\* not behave like shit with pyinstaller, but idk really.
 
 This it not guaranteed, but strongly believed by me to always be a str with valid version info...
