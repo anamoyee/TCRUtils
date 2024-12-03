724eca58409db46ac5a4d96ca3afb1a5844cf0d2
TheCreatorrr333 <thecreatorrr333@gmail.com>
2024-12-03 22:31:38 +0100

    Result added in this version

--- tcrutils/_version.py
+++ tcrutils/_version.py
@@ -1,5 +1,5 @@
 # fmt: off
-__version__ = '13.0.8'
+__version__ = '13.0.9'
 """## Now \\*should\\* not behave like shit with pyinstaller, but idk really.
 
 This it not guaranteed, but strongly believed by me to always be a str with valid version info...
