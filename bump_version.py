46a12eb48dfd9e2d8cb9b4b568f31071afc93235
TheCreatorrr333 <thecreatorrr333@gmail.com>
2025-01-31 01:44:17 +0100

    commit files not committed before upload and bump version to 13.1.0

--- bump_version.py
+++ bump_version.py
@@ -1,5 +1,6 @@
 import pathlib as p
 import re as regex
+import sys
 
 from setup import PACKAGE_NAME, VERSION_REGEX
 
@@ -7,6 +8,15 @@ version_file = p.Path(f"./{PACKAGE_NAME}/_version.py")
 
 
 def main() -> str:
+	requested_not_to_bump = any(
+		x in sys.argv
+		for x in (
+			"--don't-bump",
+			"--dont-bump",
+			"--no-bump",
+		)
+	)
+
 	content = version_file.read_text()
 
 	# Search for the version string and extract it
@@ -14,7 +24,12 @@ def main() -> str:
 	if match:
 		version_parts = match.group(1).split(".")
 		version_parts = [int(x) for x in version_parts]
-		version_parts[-1] += 1  # Increment the patch version
+
+		# Do the whole ordeal and return but dont change the number if requested not to
+
+		if not requested_not_to_bump:
+			version_parts[-1] += 1  # Increment the patch version
+
 		version_parts = [str(x) for x in version_parts]
 
 		version_after_bump = ".".join(version_parts)
@@ -28,6 +43,11 @@ def main() -> str:
 
 		# Write the updated content back to the file
 		version_file.write_text(updated_content)
+
+		if not requested_not_to_bump:
+			print(f"bumping version to {version_after_bump}")
+		else:
+			print(f"NOT bumping the version (as requested) (staying on {version_after_bump})")
 
 		return version_after_bump
 	else:
