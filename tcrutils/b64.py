127c8abb875e1814165a5974090f9fce2005e2ba
TheCreatorrr333 <thecreatorrr333@gmail.com>
2025-02-01 17:02:31 +0100

    make b64.py more sane

--- tcrutils/b64.py
+++ tcrutils/b64.py
@@ -1,14 +1,9 @@
-import base64
+import base64 as _base64
 
 
-class b64:
-	def __init__(self) -> None:
-		raise RuntimeError("This class should not be instantiated.")
+def encode(text: str, encoding: str = "utf-8") -> str:
+	return _base64.b64encode(text.encode(encoding=encoding)).decode(encoding=encoding)
 
-	@staticmethod
-	def encode(text: str, encoding: str = "utf-8") -> str:
-		return base64.b64encode(text.encode(encoding=encoding)).decode(encoding=encoding)
 
-	@staticmethod
-	def decode(text: str, encoding: str = "utf-8") -> str:
-		return base64.b64decode(text.encode(encoding=encoding)).decode(encoding=encoding)
+def decode(text: str, encoding: str = "utf-8") -> str:
+	return _base64.b64decode(text.encode(encoding=encoding)).decode(encoding=encoding)
