ffa220eb2c46749f7b591bcfbbc55c7753184204
TheCreatorrr333 <thecreatorrr333@gmail.com>
2025-02-23 20:13:59 +0100

    commit before uploading (v13.1.12)

--- tcrutils/_version.py
+++ tcrutils/_version.py
@@ -1,5 +1,5 @@
 # fmt: off
-__version__ = '13.1.11'
+__version__ = '13.1.12'
 """## Now \\*should\\* not behave like shit with pyinstaller, but idk really.
 
 This it not guaranteed, but strongly believed by me to always be a str with valid version info...
