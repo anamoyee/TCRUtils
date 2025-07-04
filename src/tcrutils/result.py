a069a9c1cfb86e98869361f912c67d3950a5b3b5
anamoyee <thecreatorrr333@gmail.com>
2025-07-04 14:11:49 +0200

    change around how result works, add ResultUnwrapDirect, fix docstrings

--- src/tcrutils/result.py
+++ src/tcrutils/result.py
@@ -14,21 +14,15 @@ class EmptyType(Enum):
 Default = TypeVar("Default")
 
 
-@dataclass
-class ResultUnwrappedOnErrorError[Err: BaseException](Exception):
+class ResultUnwrappedOnErrorError(Exception):
 	"""An unwrap() was called on a Result containing an error."""
 
-	that_error: Err
 
-
-@dataclass
-class ResultUnwrappedErrOnValueError[Ok](Exception):
+class ResultUnwrappedErrOnValueError(Exception):
 	"""An unwrap_err() was called on a Result contaning a value."""
 
-	that_value: Ok
 
-
-class Result[Ok, Err: BaseException]:
+class _ResultBase[Ok, Err: BaseException]:
 	_value: Ok | Err
 	_is_err: bool
 
@@ -73,17 +67,10 @@ class Result[Ok, Err: BaseException]:
 
 		return self._is_err == other._is_err and self._value == other._value
 
-	def unwrap(self) -> Ok:
-		"""If the result contains an error, raise ResultUnwrappedIncorrectlyError(that_error), else return the value."""
-		if self._is_err:
-			raise ResultUnwrappedOnErrorError[Err](self._value)
-
-		return self._value
-
 	def unwrap_err(self) -> Err:
-		"""If the result contains a value, raise ResultUnwrappedErrOnValueError(that_value), else return the error."""
+		"""If the result contains a value, raise `ResultUnwrappedErrOnValueError()`, else return the error."""
 		if not self._is_err:
-			raise ResultUnwrappedErrOnValueError[Ok](self._value)
+			raise ResultUnwrappedErrOnValueError() from self._value
 
 		return self._value
 
@@ -102,14 +89,14 @@ class Result[Ok, Err: BaseException]:
 		return self._value
 
 	def unwrap_or_else(self, default_factory: Callable[[Err], Default]) -> Ok | Default:
-		"""If the result contains an error, return default_factory(that_error), else return the value."""
+		"""If the result contains an error, return `default_factory(that_error)`, else return the value."""
 		if self.is_err:
 			return default_factory(self._value)
 
 		return self._value
 
 	def unwrap_err_or_else(self, default_factory: Callable[[Ok], Default]) -> Err | Default:
-		"""If the result contains a value, return default_factory(that_value), else return the error."""
+		"""If the result contains a value, return `default_factory(that_value)`, else return the error."""
 		if not self.is_err:
 			return default_factory(self._value)
 
@@ -122,12 +109,14 @@ class Result[Ok, Err: BaseException]:
 			raise self._value
 
 	def map_err(self, f: Callable[[Err], Err]) -> Self:
+		"""If the result contains an error, set it to `f(current_error)`, else do nothing."""
 		if self.is_err:
 			self._value = f(self._value)
 
 		return self
 
 	def map_ok(self, f: Callable[[Ok], Ok]) -> Self:
+		"""If the result contains a value (Ok), set it to `f(current_value)`, else do nothing."""
 		if self.is_ok:
 			self._value = f(self._value)
 
@@ -143,6 +132,36 @@ class Result[Ok, Err: BaseException]:
 			+ fmt_iterable(bool(self.is_ok)).replace("True", "Ok").replace("False", "Err")
 			+ (FMT_BRACKETS[tuple][syntax_highlighting] % fmt_iterable(self._value))
 		)
+
+	def unwrap_direct(self) -> Ok:
+		"""If the result contains an error, raise it, else return the value."""
+		if self._is_err:
+			raise self._value
+
+		return self._value
+
+	def unwrap_indirect(self) -> Ok:
+		"""If the result contains an error, raise `ResultUnwrappedOnErrorError()`, else return the value."""
+		if self._is_err:
+			raise ResultUnwrappedOnErrorError() from self._value
+
+		return self._value
+
+
+class Result[Ok, Err: BaseException](_ResultBase[Ok, Err]):
+	"""May contain a value or an error, but not both. Check with `.is_ok` or `.unwrap()`, etc."""
+
+	def unwrap(self) -> Ok:
+		"""If the result contains an error, raise `ResultUnwrappedOnErrorError()`, else return the value (On `Result` alias to `.unwrap_indirect()`)."""
+		return self.unwrap_indirect()
+
+
+class Result2[Ok, Err: BaseException](Result[Ok, Err]):
+	"""Like Result, but .unwrap() in the sad case raises the underling error, not `ResultUnwrappedOnErrorError()` (On `Result2` alias to `.unwrap_direct()`). Normal `Result`'s `.unwrap()` can also be accessed via `.unwrap_indirect()`."""
+
+	def unwrap(self) -> Ok:
+		"""If the result contains an error, raise it, else return the value."""
+		return self.unwrap_direct()
 
 
 def aresultify[**P, R](f: Callable[P, Coroutine[Any, Any, R]]) -> Callable[P, Coroutine[Any, Any, Result[R, Exception]]]:
@@ -163,5 +182,27 @@ def resultify[**P, R](f: Callable[P, R]) -> Callable[P, Result[R, Exception]]:
 			return Result.new_ok(f(*args, **kwargs))
 		except Exception as e:
 			return Result.new_err(e)
+
+	return wrapper
+
+
+def aresultify2[**P, R](f: Callable[P, Coroutine[Any, Any, R]]) -> Callable[P, Coroutine[Any, Any, Result2[R, Exception]]]:
+	@wraps(f)
+	async def wrapper(*args, **kwargs):
+		try:
+			return Result2.new_ok(await f(*args, **kwargs))
+		except Exception as e:
+			return Result2.new_err(e)
+
+	return wrapper
+
+
+def resultify2[**P, R](f: Callable[P, R]) -> Callable[P, Result2[R, Exception]]:
+	@wraps(f)
+	def wrapper(*args, **kwargs):
+		try:
+			return Result2.new_ok(f(*args, **kwargs))
+		except Exception as e:
+			return Result2.new_err(e)
 
 	return wrapper
